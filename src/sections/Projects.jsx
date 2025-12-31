82ca15c8344339933237dd9723e369c2432f6bf3
DariushOps <overlord127001@gmail.com>
2025-12-31 19:01:59 +0330

    update

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,3 +1,7 @@
 export default function Projects() {
-  return <section></section>;
+  return (
+    <section id="about" className="py-32 overflow-hidden relative">
+      <div className="relative container mx-auto z-10 px-6"></div>
+    </section>
+  );
 }
